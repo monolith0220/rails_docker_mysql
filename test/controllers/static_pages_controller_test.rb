46a58d3ccaf3c9a80ddcea2dff3125945e18f697
monolith0220 <funky.atmos.worlddd@gmail.com>
2023-07-05 16:21:50 +0900

    静的ページ作成完了

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,13 +1,41 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
+
+  # 各テストが走る前に実行されるメソッド
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get root" do
+    get root_url
     assert_response :success
+  end
+
+  test "should get home" do
+    # GETリクエストをhomeアクションに対して発行
+    get static_pages_home_url
+    # リクエストに対するレスポンスは[成功]か
+    assert_response :success
+    # titleというタグが存在するか
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | #{@base_title}"
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | #{@base_title}"
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 end
