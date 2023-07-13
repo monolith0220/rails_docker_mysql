18ecdf7a60c5532903e3a359c163e00c74fe594a
monolith0220 <funky.atmos.worlddd@gmail.com>
2023-07-13 11:12:09 +0900

    ユーザー登録最初のURL

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root 'static_pages#home'
   # get 'static_pages/home'
   #getリクエストが/helpに送信されたとき,static_pagesコントローラーのhelpアクションが呼び出される
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/signup', to: 'users#new'
 end
