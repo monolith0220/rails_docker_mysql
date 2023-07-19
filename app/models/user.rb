f9ea97cc751163c1529f10c8529ef4541001ec2d
monolith0220 <funky.atmos.worlddd@gmail.com>
2023-07-19 16:05:23 +0900

    存在性を検証

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+	# name属性の存在性に関するテスト
+	validates :name, presence: true
+	validates :email, presence: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -8,7 +8,18 @@ class UserTest < ActiveSupport::TestCase
     @user = User.new(name: "Lionel Messhi", email: "messhi@fifa.com")
   end
 
+  # userオブジェクトが有効かテスト
   test "should be valid" do
     assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = "    "
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "    "
+    assert_not @user.valid?
   end
 end
