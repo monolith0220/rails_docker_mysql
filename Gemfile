625f35f8f89df518a94f7f4d6d848ae378608a8a
monolith0220 <funky.atmos.worlddd@gmail.com>
2023-07-26 17:30:05 +0900

    ハッシュ化されたパスワード

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ ruby "3.1.4"
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.6"
 gem 'bootstrap-sass'
+gem 'bcrypt'
 
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
 	# email属性を強制的に小文字
 	before_save {email.downcase!}
-	# name属性の存在性に関するテスト
+	# name属性の存在性に関するテスト presence: 必須
 	validates :name, presence: true, length: {maximum:50}
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, length: {maximum:255}, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+	has_secure_password
 end
