c319e4f4fae4d8a533f7faa9f5209a373f7f4298
monolith0220 <funky.atmos.worlddd@gmail.com>
2023-07-26 15:42:36 +0900

    一意性を検証する

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
+	# email属性を強制的に小文字
+	before_save {email.downcase!}
 	# name属性の存在性に関するテスト
 	validates :name, presence: true, length: {maximum:50}
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates :email, presence: true, length: {maximum:255}, format: {with: VALID_EMAIL_REGEX}
+	validates :email, presence: true, length: {maximum:255}, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -54,4 +54,21 @@ class UserTest < ActiveSupport::TestCase
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
   end
+
+  # 重複するメールアドレス拒否のテスト
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    # 大文字を区別しない
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  # メールアドレスを小文字化するテスト
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
 end
